e042661a24be4ec9c89ce1df5094b20412cf471e
Valentin Toma <valentin.toma@nc-vision.com>
2023-10-12 10:38:10 +0300

    Add local project to repo

---
+++ src/CustomMap.ts
@@ -0,0 +1,42 @@
+// Instruction to every other class
+// on how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
+const contentString = '<div>"Hello World"</div>';
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        center: { lat: 0, lng: 0 },
+        zoom: 3,
+      }
+    );
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+
+    marker.addListener("click", () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
